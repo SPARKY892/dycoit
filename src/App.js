a5f872207699d8485a6c73183a366ef0a6cd0674
Mark <marksmellie892@gmail.com>
2021-05-08 17:32:20 +0100

    Added Footer component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import ContactForm from './components/ContactForm';
 import Header from './components/Header';
 import HomePage from './components/HomePage';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div className="App">                 
         <Header />     
         <HomePage />
+        <Footer />
     </div>
   );
 }
